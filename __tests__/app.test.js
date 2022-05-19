c867d1be8dc6c2f5bce247ca11d82e18a9d9df5d
bashirzadehdanial <danial@Danials-MacBook-Pro.local>
2022-05-19 12:43:22 +0100

    updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const seed= require('../db/seeds/seed.js');
 const data= require('../db/data/test-data');
 const request= require('supertest');
 const app = require('../app.js');
-const { response } = require('../app.js');
+
 
 
 
